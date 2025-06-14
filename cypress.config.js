fc0dc22bf9fd2b0382b536d2a4eadcbd284b471e
Albar <albareizy942@gmail.com>
2025-06-14 13:20:23 +0700

    update fix error

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,31 @@
+// const { defineConfig } = require('cypress');
+
+// module.exports = defineConfig({
+//   reporter: 'cypress-mochawesome-reporter',
+//   reporterOptions: {
+//     charts: true,
+//     reportPageTitle: 'Soapleasure Tests Report',
+//     embeddedScreenshots: true,
+//     inlineAssets: true,
+//   },
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       require('cypress-mochawesome-reporter/plugin')(on);
+//       return config;
+//     },
+//     baseUrl: 'https://soapleasure.com/',
+//   },
+// });
+
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
+    reportDir: 'cypress/reports/mocha',
+    overwrite: true,
+    html: true,
+    json: true,
     charts: true,
     reportPageTitle: 'Soapleasure Tests Report',
     embeddedScreenshots: true,
